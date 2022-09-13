cdb5d53570629742fb0e15f55a0dafef211cc7ac
c1323669 <bomartins@stefanini.com>
2022-09-13 00:11:15 -0300

    Implementação do serviço de CEP e envio do formulário via post

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Form, FormControl, FormGroup, NgForm, NgModel } from '@angular/forms';
+import { map } from 'rxjs';
+
 
 @Component({
   selector: 'app-template-form',
@@ -12,23 +16,76 @@ export class TemplateFormComponent implements OnInit {
     email: null
   }
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit(form : any){
-    console.log(form);
+  onSubmit(form: NgForm) {
+
+    this.http.post('/formulario', JSON.stringify(form.value))
+      .subscribe(dados => console.log(dados));
+    console.log(JSON.stringify(form.value));
   }
 
-  verificaValidTouched(campo: any){
+  consultaCep(cep: string, form: NgForm) {
+    console.log(cep);
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != "") {
+      var validacep = /^[0-9]{8}$/;
+      if (validacep.test(cep)) {
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
+          .subscribe(dados => {
+            this.populaDadosForm(dados, form);
+          })
+      }
+    }
+
+
+  }
+
+  populaDadosForm(dados: any, form: NgForm) {
+    /*     form.setValue({
+          nome: form.value.nome,
+          email: form.value.email,
+          endereco: {
+            rua: dados.logradouro,
+            cep: dados.cep,
+            numero: '',
+            complemento: dados.complemento,
+            bairro: dados.bairro,
+            cidade: dados.localidade,
+            estado: dados.uf
+          }
+        }) */
+
+    form.form.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+
+    })
+  }
+
+  verificaInValidTouched(campo: any) {
     return !campo.valid && campo.touched;
   }
 
-  aplicaCssErro(campo: any){
+  verificaDirtyValid(campo: any) {
+    return campo.valid && campo.dirty;
+  }
+
+  aplicaCssErro(campo: any) {
     return {
-      'is-valid': this.verificaValidTouched(campo),
-      'is-invalid': this.verificaValidTouched(campo)
+      'is-valid': this.verificaDirtyValid(campo),
+      'is-invalid': this.verificaInValidTouched(campo)
     }
   }
 
--- src/app/template-form/template-form.module.ts
+++ src/app/template-form/template-form.module.ts
@@ -1,17 +1,21 @@
+import { FormDebugComponent } from './../form-debug/form-debug.component';
 import { FormsModule } from '@angular/forms';
 import { TemplateFormComponent } from './template-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [
-    TemplateFormComponent
+    TemplateFormComponent,
+    FormDebugComponent
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class TemplateFormModule { }
